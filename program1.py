4e9bc29e1f0622065e511010494b629fc64dda0a
WingsumSiu <wingsum.hackthehood.org>
2021-06-15 11:14:46 -0700

    [LeBron and car]

---
+++ program1.py
@@ -0,0 +1,20 @@
+print("hello world")
+
+name = "Lebron"
+# name is a string
+age = 35
+# age is an integer
+salary = 37.44
+# salary is an integer
+plays_basketball = True
+# plays_basketball is a boolean
+jersey_number = "23"
+# jersey_number is a String
+
+car_name = "Toyota"
+x = 50
+my_first_name = "Wing Sum"
+
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
