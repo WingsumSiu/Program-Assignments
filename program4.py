b36ac6a149ce145974eee747c70a584904628de0
WingsumSiu <wingsum.hackthehood.org>
2021-06-22 19:19:22 -0700

    Function notes

--- program4.py
+++ program4.py
@@ -1,3 +1,13 @@
+def fibonacciFinder(max):
+    i = 0
+    j = 1
+    k = 0
+    while i < max:
+        print(k)
+        k = i + j
+        i = j
+        j = k
+
 for i in range (2, 100):
     for j in range (2,3):
         if i % j == 0:
@@ -26,3 +36,26 @@ for b in range(0,5):
 
 print(areaList)
 
+# function with a tuple as the parameter
+#def foodSum2(Dict, *args):
+    #for keys in args:
+        #print(Dict[keys])
+
+#foodSum2(Menu, "Burgers")
+
+#def foodSum2(Dict, *args):
+    #for keys in args:
+        #if keys in args:
+            #sum += Dict[keys]
+
+#foodSum2(Menu, "Burgers")
+
+#list1 = [1, 2, 3, 4, 5]
+#list2 = [10, 11, 12, 13, 14]
+#difflist = []
+
+# for i in list1:
+    #for j in list2:
+        #diffList.append(abs(i - j))
+#diffList.sort()
+#print(diffList[0])
