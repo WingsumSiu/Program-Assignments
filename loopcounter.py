4a10728eca96e3efcb9ab6bbb7a76ffc4a3bcc0c
WingsumSiu <wingsum.hackthehood.org>
2021-06-28 15:22:24 -0700

    Added input line

--- loopcounter.py
+++ loopcounter.py
@@ -2,6 +2,7 @@
 def loopCounterF(value):
     loopCounter = 20
     for x in range (0, loopCounter):
+        # value = input("Enter a number between 1 and 10")
         if value > 0 and value < 11:
             # calculate = value * 3
             print(value * 3)
