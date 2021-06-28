bb440c46b3c0d0953685a813260411fcbf877b80
WingsumSiu <wingsum.hackthehood.org>
2021-06-28 12:19:15 -0700

    loopCounter to 20

--- loopcounter.py
+++ loopcounter.py
@@ -1,6 +1,6 @@
 # F for function
 def loopCounterF(value):
-    loopCounter = 19
+    loopCounter = 20
     for x in range (0, loopCounter):
         if value > 0 and value < 11:
             # calculate = value * 3
